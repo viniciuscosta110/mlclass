da05b7fbcd0e9c6ef007e3657557789f293d2a29
viniciuscosta110 <viniciuscosta1900@hotmail.com>
2024-01-28 21:09:47 -0300

    feat: final validation activity commit

--- 03_Validation/codevinci.py
+++ 03_Validation/codevinci.py
@@ -3,30 +3,25 @@ import requests
 import pandas as pd
 import numpy as np
 
+from sklearn.metrics import accuracy_score, classification_report
 from sklearn.model_selection import StratifiedShuffleSplit
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.svm import SVC
-from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
-from sklearn.ensemble import IsolationForest, RandomForestClassifier
-from sklearn.model_selection import cross_val_score
 from sklearn.preprocessing import StandardScaler
-
-import seaborn as sns
+from sklearn.svm import SVC
 
 numerical_cols = [
     'length', 'diameter', 'height', 
     'whole_weight', 'shucked_weight', 'viscera_weight', 
-    'shell_weight', 'height_times_shell_weight', 'length_plus_shell_weight', 
-    'diameter_times_shell_weight'
+    'shell_weight', 'height_t_shell', 'length_p_shell', 
+    'diameter_t_shell'
 ]
 
 def load_data(file_path='abalone_dataset.csv'):
     return pd.read_csv(file_path)
 
 def preprocess_data(df):
-    df['height_times_shell_weight'] = (df['height'] * df['shell_weight'])
-    df['length_plus_shell_weight'] = df['length'] + df['shell_weight']
-    df['diameter_times_shell_weight'] = df['diameter'] + df['shell_weight']
+    df['height_t_shell'] = (df['height'] * df['shell_weight'])
+    df['length_p_shell'] = df['length'] + df['shell_weight']
+    df['diameter_t_shell'] = df['diameter'] * df['shell_weight']
 
     # Use OneHotEncoder for categorical columns
     df = pd.get_dummies(df, columns=['sex'], prefix=['sex'])
@@ -56,108 +51,39 @@ def split_data(X, y):
     
     return X_train, X_test, y_train, y_test
 
-def train_isolation_forest(X_train):
-    isolation_forest = IsolationForest(contamination=0.1)
-    outliers = isolation_forest.fit_predict(X_train)
-    return isolation_forest, outliers
-
 def evaluate_model(model, X, y):
     y_pred = model.predict(X)
     accuracy = accuracy_score(y, y_pred)
     print(f"Acurácia: {accuracy}")
     return y_pred
 
-def train_knn_after_forest(X_train, y_train, outliers):
-    # Remove outliers from training data
-    X_train_cleaned = X_train
-    y_train_cleaned = y_train
-
-    # Train KNN classifier
-    knn_classifier = KNeighborsClassifier(n_neighbors=17)
-    knn_classifier.fit(X_train_cleaned, y_train_cleaned)
-    return knn_classifier
-
-def train_svc_after_forest(X_train, y_train, outliers):
-    # Remove outliers from training data
-    X_train_cleaned = X_train
-    y_train_cleaned = y_train
-
+def train_svc_after_forest(X_train, y_train):
     # Train SVM classifier
     svm_classifier = SVC(random_state=42, kernel='rbf', C=100, gamma='auto', probability=True, break_ties=True, class_weight='balanced')
-    svm_classifier.fit(X_train_cleaned, y_train_cleaned)
+    svm_classifier.fit(X_train, y_train)
     return svm_classifier
-
-def train_rf_classifier(X_train, y_train, outliers):
-
-    # Remove outliers from training data
-    X_train_cleaned = X_train
-    y_train_cleaned = y_train
-
-    # Train Random Forest classifier
-    rf_classifier = RandomForestClassifier(n_estimators=120, random_state=2048)
-    rf_classifier.fit(X_train_cleaned, y_train_cleaned)
-    return rf_classifier
 
 # Load data
 df = load_data()
 
 # Preprocess data
-preProcess_df = preprocess_data(df)
+preprocessed_df = preprocess_data(df)
 
 # Separate features and target variable
-X = preProcess_df.drop('type', axis=1)
-y = preProcess_df['type']
+X = preprocessed_df.drop('type', axis=1)
+y = preprocessed_df['type']
 
 # Split data
 X_train, X_test, y_train, y_test = split_data(X, y)
 
-isolation_forest, outliers = train_isolation_forest(X_train)
-svm_classifier = train_svc_after_forest(X_train, y_train, outliers)
-"""
-knn_classifier = train_knn_after_forest(X_train, y_train, outliers)
-rf_classifier = train_rf_classifier(X_train, y_train, outliers)
-"""
+svm_classifier = train_svc_after_forest(X_train, y_train)
 
-# Train SVM classifier
-svm_scores = cross_val_score(svm_classifier, X_train, y_train, cv=10)
-svm_accuracy = accuracy_score(y_test, svm_classifier.predict(X_test))
-svm_best_accuracy = max(svm_scores)
+y_pred = evaluate_model(svm_classifier, X_test, y_test)
 
-""" # Train KNN classifier
-knn_scores = cross_val_score(knn_classifier, X_train, y_train, cv=10)
-knn_accuracy = accuracy_score(y_train, knn_classifier.predict(X_train))
-knn_best_accuracy = max(knn_scores)
-
-# Train Random Forest classifier
-rf_scores = cross_val_score(rf_classifier, X_train, y_train, cv=10)
-rf_accuracy = accuracy_score(y_train, rf_classifier.predict(X_train))
-rf_best_accuracy = max(rf_scores) """
-
-# Find the best model
-models = [
-    ('SVM', svm_best_accuracy, svm_classifier),
-"""     ('KNN', knn_best_accuracy, knn_classifier), """
-"""     ('RandomForest', rf_best_accuracy, rf_classifier) """
-]
-
-best_model = max(models, key=lambda x: x[0])
-best_classifier_name, best_accuracy, best_classifier = best_model
-
-
-y_pred = evaluate_model(best_classifier, X_test, y_test)
-cm = confusion_matrix(y_test, y_pred)
 classification_report = classification_report(y_test, y_pred)
 print(classification_report)
-""" # Plot the confusion matrix
-plt.figure(figsize=(8, 6))
-sns.heatmap(cm, annot=True, fmt="d", cmap="Blues", xticklabels=best_classifier.classes_, yticklabels=best_classifier.classes_)
-plt.title("Confusion Matrix")
-plt.xlabel("Predicted")
-plt.ylabel("True")
-plt.show() """
-""" print(f"Best model: {best_classifier} with accuracy: {best_accuracy}") """
-  
-  # Enviando previsões realizadas com o modelo para o servidor
+
+# Enviando previsões realizadas com o modelo para o servidor
 URL = "https://aydanomachado.com/mlclass/03_Validation.php"
 
 
@@ -166,7 +92,7 @@ DEV_KEY = "Codevinci"
 
 data_app = load_data('abalone_app.csv')
 data_app = preprocess_data(data_app)
-predicted_data = best_classifier.predict(data_app)
+predicted_data = svm_classifier.predict(data_app)
 
 # json para ser enviado para o servidor
 data = {'dev_key':DEV_KEY,
@@ -177,4 +103,4 @@ r = requests.post(url = URL, data = data)
 
 # Extraindo e imprimindo o texto da resposta
 pastebin_url = r.text
-print(" - Resposta do servidor:\n", r.text, "\n") 
+print(" - Resposta do servidor:\n", r.text, "\n")
